b65c137ac24c527c72d1bbf2db7a83042ce17d76
tomasCalletce <64665732+tomasCalletce@users.noreply.github.com>
2023-03-04 12:14:54 -0500

    make unisawp test

--- test/Unisawp.sol
+++ test/Unisawp.sol
@@ -13,12 +13,13 @@ import "forge-std/console.sol";
       of possible periphery-like contracts. However, they are useful examples of how to safely 
       and efficiently interact with Uniswap V2.
    */
+import { IUniswapV2Router02 } from "v2-periphery/interfaces/IUniswapV2Router02.sol";
 
 contract Unisawp is Test {
 
-    address UniswapV2Router02 = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    IUniswapV2Router02 UniswapV2Router02 = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+
     address UniswapV2Factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
-    
     uint256 mainnetFork;
 
     constructor(){
@@ -33,6 +34,8 @@ contract Unisawp is Test {
     function testCanGetCorrectFactoryAddress() public {
         vm.selectFork(mainnetFork);
 
+        address uniswapV2Factory = UniswapV2Router02.factory();
+        assertEq(uniswapV2Factory, UniswapV2Factory);
     }
 
 
